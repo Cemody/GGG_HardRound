cd1593bcb57f2f09131d0c9210e5377efb64f197
Cemody <85206469+Cemody@users.noreply.github.com>
2021-06-02 11:56:18 +0800

    Add files via upload

---
+++ Hard1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progcomp;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Dexter Dela Rosa
+ */
+public class Hard1 {
+    public static void main(String[] args) {
+        BigDecimal d = new BigDecimal(Math.PI);
+        System.out.println(d);
+
+    }
+}
