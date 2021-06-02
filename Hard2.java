482df41b9321f63cefd4a23fc6d406daab1697ea
ABSL-118 <73323375+ABSL-118@users.noreply.github.com>
2021-06-02 11:56:08 +0800

    Add files via upload

---
+++ Hard2.java
@@ -0,0 +1,37 @@
+package progcompe;
+
+public class Hard2 {
+
+    public static void main(String[] args) {
+        String[] list = {"Hello", "World", "in", "a", "frame"};
+        frame(list);
+    }
+
+    public static void frame(String[] list) {
+        int size = list.length;
+        for (int j = 0; j < maxLength(list)+2; j++) {
+                    System.out.print("*");
+                }
+        System.out.println("");
+        for (int i = 0; i < size; i++) {
+            while(list[i].length()!= maxLength(list)) list[i]= list[i]+" ";
+            System.out.println("*" + list[i] + "*");
+        }
+        for (int j = 0; j < maxLength(list) + 2; j++) {
+            System.out.print("*");
+        }
+    }
+
+    static int maxLength(String[] arr) {
+        int len = Integer.MIN_VALUE;
+        int N = arr.length;
+
+        for (int i = 0; i < N; i++) {
+            int l = arr[i].length();
+            if (len < l) {
+                len = l;
+            }
+        }
+        return len;
+    }
+}
